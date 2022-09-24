9a6b13db8b71e3ccd97c4e88f42e449b82a8668f
Asutosh Ranjan <asutoshranjan.work@gmail.com>
2022-09-24 22:34:25 +0530

    V_1.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,14 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:schedule/screen/about_page.dart';
 import 'package:schedule/screen/activities_page.dart';
 import 'package:schedule/screen/exams_page.dart';
 import 'package:schedule/screen/home_page.dart';
 import 'package:schedule/screen/notice_page.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-
-const AndroidNotificationChannel channel = AndroidNotificationChannel(
-    'high_importance_channel', // id
-    'High Importance Notifications', // title
-    description: 'This channel is used for important notifications', // description
-    importance: Importance.max,
-    playSound: true);
-
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-    FlutterLocalNotificationsPlugin();
-
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  await Firebase.initializeApp();
-  print("A BG massage just showed up : ${message.messageId}");
-}
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-
-  await flutterLocalNotificationsPlugin
-      .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
-      ?.createNotificationChannel(channel);
-
-  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
-    alert: true,
-    badge: true,
-    sound: true,
-  );
-
   runApp(const MyApp());
 }
 
@@ -52,50 +21,6 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
 
-  @override
-  void initState() {
-    super.initState();
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      RemoteNotification? notification = message.notification;
-      AndroidNotification? android = message.notification?.android;
-      if (notification != null && android != null){
-        flutterLocalNotificationsPlugin.show(
-          notification.hashCode.toInt(),
-          notification.title.toString(),
-          notification.body.toString(),
-          NotificationDetails(
-            android: AndroidNotificationDetails(
-              channel.id,
-              channel.name,
-              channelDescription: channel.description,
-              color: Colors.blue,
-              playSound: true,
-            ),
-          ),
-        );
-      }
-    });
-
-    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-      RemoteNotification? notification = message.notification;
-      AndroidNotification? android = message.notification?.android;
-      if(notification != null && android != null) {
-        showDialog(context: context, builder:(_) {
-          return AlertDialog(
-            title: Text(notification.title.toString()),
-            content: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(notification.body.toString()),
-                ],
-              ),
-            ),
-          );
-        });
-      }
-    });
-  }
 
 
   @override
